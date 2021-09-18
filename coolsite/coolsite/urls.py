2a8c29c1f871c6486c0eeae99ef33676627496bc
greatlaki <greatlaki911@gmail.com>
2021-09-18 16:50:28 +0300

    using method include

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import path
 
 from women.views import *
+from django.urls import path, include
 
 urlpatterns = {
     path('admin/', admin.site.urls),
-    path('women/', index),
-    path('cats/', categories),
+    path('women/', include('women.urls')),
 }
