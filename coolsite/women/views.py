8ccc7178ae16c164428f8d746393dd4e5970eca4
greatlaki <greatlaki911@gmail.com>
2021-09-19 18:11:54 +0300

    Creating 301 and 302 redirects

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 
@@ -15,7 +15,7 @@ def categories(request, catid):
 
 def archive(request, year):
     if int(year) > 2020:
-        raise Http404()
+        return redirect('home', permanent=True)
 
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
