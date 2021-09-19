173f507a96380faf2406a06415eb96e39ea6777d
greatlaki <greatlaki911@gmail.com>
2021-09-19 17:57:49 +0300

    Exception handling for requests to the server

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j=x+j(yj2!9z&y$#ps$ma6gj#zjzywuec+ofo^4+t$*ab+gm)2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 
 def index(request): #HttpRequest
@@ -14,4 +14,11 @@ def categories(request, catid):
 
 
 def archive(request, year):
+    if int(year) > 2020:
+        raise Http404()
+
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
