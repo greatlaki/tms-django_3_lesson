715c3f82ea31af067e3962eef6502d289726399b
greatlaki <greatlaki911@gmail.com>
2021-09-20 13:50:01 +0300

    Working with templates

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 
 def index(request):
-    return render(request, 'women/index.html')
+    return render(request, 'women/index.html', {'title': 'Главная страница'})
+
+
+def about(request):
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
 
 def categories(request, catid):
