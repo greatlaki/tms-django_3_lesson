c36f32c5e8282aa4bc6dcc94f16ba6d9cfabec22
greatlaki <greatlaki911@gmail.com>
2021-09-23 22:45:38 +0300

    Add new function and their URL

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -5,4 +5,7 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
+    path('about/', add_page, name='add_page'),
+    path('about/', contact, name='contact'),
+    path('about/', login, name='login'),
 ]
