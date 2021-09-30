05e89956fe450d6e6b2f02d4f8985974ba90baac
greatlaki <greatlaki911@gmail.com>
2021-09-30 19:10:07 +0300

    add class WomenAdmin - admin.py

--- coolsite/women/admin.py
+++ coolsite/women/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import  *
+from .models import *
 
-admin.site.register(Women)
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'photo', 'is_published') # Список полей, которые видна в админ-панели
+    list_display_links = ('id', 'title') # Поля, на которые можно перейти
+    search_fields = ('title', 'content') # Поиск информации
+
+
+admin.site.register(Women, WomenAdmin)
 # Register your models here.
