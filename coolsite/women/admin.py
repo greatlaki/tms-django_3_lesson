7e995ddd8f8e54fd8b820ef421b510126d2aa9f5
greatlaki <greatlaki911@gmail.com>
2021-09-30 20:20:52 +0300

    add list_editabla and filter

--- coolsite/women/admin.py
+++ coolsite/women/admin.py
@@ -6,6 +6,8 @@ class WomenAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'time_create', 'photo', 'is_published') # Список полей, которые видна в админ-панели
     list_display_links = ('id', 'title') # Поля, на которые можно перейти
     search_fields = ('title', 'content') # Поиск информации
+    list_editable = ('is_published',) # Список статей, которые можно редактировать
+    list_filter = ('is_published', 'time_create')
 
 
 class CategoryAdmin(admin.ModelAdmin):
