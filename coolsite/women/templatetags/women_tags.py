06e679fae860c7ab788b295cb9a60d3269aa7042
greatlaki <greatlaki911@gmail.com>
2021-09-30 23:35:20 +0300

    The templates flew off, the attempt to fix everything was unsuccessful

--- coolsite/women/templatetags/women_tags.py
+++ coolsite/women/templatetags/women_tags.py
@@ -3,20 +3,18 @@ from women.models import *
 
 register = template.Library()
 
-
 @register.simple_tag(name='getcats')
 def get_categories(filter=None):
     if not filter:
         return Category.objects.all()
     else:
-        return Category.objects.filter(pk=filter) # Коллекция данных
-
+        return Category.objects.filter(pk=filter)
 
 @register.inclusion_tag('women/list_categories.html')
 def show_categories(sort=None, cat_selected=0):
     if not sort:
         cats = Category.objects.all()
     else:
-        cats = Category.objects.order_by(sort) # Читает все категории
+        cats = Category.objects.order_by(sort)
 
-    return {"cats": cats, 'cat_selected': cat_selected}
+    return {"cats": cats, "cat_selected": cat_selected}
