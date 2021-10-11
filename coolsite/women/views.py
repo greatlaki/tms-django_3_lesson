e8c8a0fb6407115c1d02dfa78e7d73bbc51f3d63
greatlaki <greatlaki911@gmail.com>
2021-10-11 21:06:41 +0300

    I wrote the code for the "about" view function, which allows you to display individual pages

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
@@ -10,7 +11,7 @@ from .utils import *
 
 
 class WomenHome(DataMixin, ListView):
-    paginate_by = 3
+    paginate_by = 3 # Количесвто элементов списка на страницу
     model = Women # выбирает все записи из таблицы и пытается отобразить в виде списка
     template_name = 'women/index.html'
     context_object_name = 'posts'
@@ -36,7 +37,13 @@ class WomenHome(DataMixin, ListView):
 #   return render(request, 'women/index.html', context=context)
 
 def about(request):
-    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+    # Вывод элементов списка на странице, для функции
+    contact_list = Women.objects.all()
+    paginator = Paginator(contact_list, 3)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'women/about.html', {'page_obj':page_obj, 'menu': menu, 'title': 'О сайте'})
 
 class AddPage(LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
