9cc372036f18ef6f25d71343ed88d330faabe19e
greatlaki <greatlaki911@gmail.com>
2021-10-12 21:30:25 +0300

    The "Login User" view class for user authorization

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -144,3 +144,12 @@ class RegisterUser(DataMixin, CreateView):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title="Регистрация")
         return dict(list(context.items()) + list(c_def.items()))
+
+class LoginUser(DataMixin, ListView):
+    form_class = AuthenticationForm
+    template_name = 'women/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(c_def.items()))
